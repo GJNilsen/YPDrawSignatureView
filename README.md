6d36c99ca1e4a070a983391d9c4e899a3564a7f0
Geert-Jan Nilsen <gert@yuppielabel.com>
2014-11-07 00:27:51 +0100

    First Commit

--- README.md
+++ README.md
@@ -1,4 +1,22 @@
-YPDrawSignatureView
-===================
+# YPDrawSignatureView
 
-Capture signature view in Swift
+I could not find a good signature capturer iOS made in Swift, so I gave it a try. I leaned heavily on the blog entry [Capture a Signature on iOS](https://www.altamiracorp.com/blog/employee-posts/capture-a-signature-on-ios), and I thank Jason Harwig for his work.
+
+
+## Usage
+
+Add a new `view` where you want the signature capture field. Set its class to `YPDrawSignatureView`, and connect it to an `IBOutlet` property in your `UIViewController` (or whatever else).
+
+
+## Installation
+
+Add YPDrawSignature.swift to your project
+
+## Author
+
+Geert-Jan Nilsen ( gert@yuppielabel.com )
+[Yuppielabel](http://yuppielabel.com)
+
+## License
+
+YPDrawSignatureView is available under the MIT license. See the LICENSE file for more info.
---
+++ source/YPDrawSignatureView.swift
@@ -0,0 +1,109 @@
+//
+//  YPDrawSignatureView.swift
+//  drawtest
+//
+//  Created by Geert-Jan Nilsen on 06/11/14.
+//  Copyright (c) 2014 Yuppielabel.com All rights reserved.
+//
+
+import UIKit
+
+class BetterSignatureView: UIView {
+    
+    // MARK: - Properties
+    var path = UIBezierPath()
+    var pts: [CGPoint] = [CGPoint(),CGPoint(),CGPoint(),CGPoint(),CGPoint()]
+    var ctr: Int = 0
+    
+    // MARK: - Init
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        // Capturing Touches
+        // DrawTouch
+        var pan: UIPanGestureRecognizer = UIPanGestureRecognizer(target:self, action:"pan:")
+        pan.maximumNumberOfTouches = 1
+        pan.minimumNumberOfTouches = 1
+        pan.cancelsTouchesInView = true
+        self.addGestureRecognizer(pan)
+        
+        // Dotting-The-I's-Touches
+        var tap: UITapGestureRecognizer = UITapGestureRecognizer(target:self, action:"tap:")
+        pan.cancelsTouchesInView = true
+        self.addGestureRecognizer(tap)
+        
+        
+        self.backgroundColor = UIColor.whiteColor()
+        self.path.lineWidth = 2.0
+    }
+    
+    // MARK: - Draw
+    override func drawRect(rect: CGRect) {
+        self.path.stroke()
+    }
+    
+    // MARK: - Gesture Recognizers
+    
+    // MARK: Pan - Draws the Signature Curves
+    func pan(touch: UIPanGestureRecognizer){
+        let touchPoint: CGPoint = touch.locationInView(self)
+        
+        if (touch.state == UIGestureRecognizerState.Began) {
+            
+            self.ctr = 0
+            self.pts[0] = touchPoint
+            
+        } else if (touch.state == UIGestureRecognizerState.Changed) {
+            
+            self.ctr++
+            self.pts[self.ctr] = touchPoint
+            if (self.ctr == 4) {
+                self.pts[3] = CGPointMake((self.pts[2].x + self.pts[4].x)/2.0, (self.pts[2].y + self.pts[4].y)/2.0)
+                self.path.moveToPoint(self.pts[0])
+                self.path.addCurveToPoint(self.pts[3], controlPoint1:self.pts[1], controlPoint2:self.pts[2])
+                
+                self.setNeedsDisplay()
+                self.pts[0] = self.pts[3]
+                self.pts[1] = self.pts[4]
+                self.ctr = 1
+            }
+            
+            self.setNeedsDisplay()
+            
+        } else if (touch.state == UIGestureRecognizerState.Ended || touch.state == UIGestureRecognizerState.Cancelled) {
+            
+            //self.path.moveToPoint(touchPoint)
+            self.path.addLineToPoint(touchPoint)
+            self.ctr = 0
+            self.setNeedsDisplay()
+            
+        }
+        
+    }
+    
+    // MARK: Tap - Draws the Dots
+    func tap(touch: UITapGestureRecognizer){
+        var touchPoint: CGPoint = touch.locationInView(self)
+        self.path.moveToPoint(CGPointMake(touchPoint.x-1.0,touchPoint.y))
+        self.path.addLineToPoint(CGPointMake(touchPoint.x+1.0,touchPoint.y))
+        self.setNeedsDisplay()
+    }
+    
+    // MARK: - Helpers
+    
+    // MARK: Clear the Signature View
+    func clearSignature() {
+        self.path.removeAllPoints()
+        self.setNeedsDisplay()
+    }
+    
+    // MARK: Save the Signature as an UIImage
+    func getSignature() ->UIImage {
+        UIGraphicsBeginImageContext(CGSizeMake(self.bounds.size.width, self.bounds.size.height))
+        self.layer.renderInContext(UIGraphicsGetCurrentContext())
+        var signature: UIImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return signature
+    }
+    
+}
