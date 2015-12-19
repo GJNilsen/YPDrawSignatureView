5306f0cd189f209e2cf7ac49764269f52aa2587b
GJ Nilsen <gert@yuppielabel.com>
2015-12-19 10:20:35 +0100

    Update ViewController.swift
    Added some comments.

--- SignatureTest/SignatureTest/ViewController.swift
+++ SignatureTest/SignatureTest/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  // Connect this Outlet to the Signature View
   @IBOutlet weak var drawSignatureView: YPDrawSignatureView!
   
   override func viewDidLoad() {
@@ -23,11 +24,19 @@ class ViewController: UIViewController {
   }
 
   @IBAction func clearSignature(sender: UIButton) {
+    // This is how the signature gets cleared
     self.drawSignatureView.clearSignature()
   }
   
   @IBAction func saveSignature(sender: UIButton) {
-    UIImageWriteToSavedPhotosAlbum(self.drawSignatureView.getSignature(), nil, nil, nil)
+    // Getting the Signature Image from self.drawSignatureView using the method getSignature().
+    let signatureImage = self.drawSignatureView.getSignature()
+    
+    // Saving signatureImage from the line above to the Photo Roll.
+    // The first time you do this, the app asks for access to your pictures.
+    UIImageWriteToSavedPhotosAlbum(signatureImage, nil, nil, nil)
+    
+    // Since the Signature is now saved to the Photo Roll, the View can be cleared anyway.
     self.drawSignatureView.clearSignature()
   }
   
