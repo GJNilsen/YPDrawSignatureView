1bb3b9f760a3acf711c8d2ede0a526448b090142
Cyrille Legrand <63708265+cyrillelegrand@users.noreply.github.com>
2020-06-04 12:14:46 +0200

    Support Swift Tools version 5 (#81)

--- Package.swift
+++ Package.swift
@@ -1,5 +1,18 @@
+// swift-tools-version:5.0
 import PackageDescription
 
 let package = Package(
-    name: "YPDrawSignature"
+    name: "YPDrawSignature",
+    platforms: [.iOS(.v11)],
+    products: [
+        .library(name: "YPDrawSignature", targets: ["YPDrawSignature"])
+    ],
+    targets: [
+      .target(
+        name: "YPDrawSignature",
+        path: "",
+        sources: ["Sources"]
+      )
+    ],
+    swiftLanguageVersions: [.v5]
 )
